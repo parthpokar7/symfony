de3dc2fc46e93d0abbb52eae70981c8f9cd94045
parthpokar7 <parthpokar.77@gmail.com>
2022-07-01 00:24:50 +0530

    Handelling forms

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -9,6 +9,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
@@ -41,6 +42,10 @@ class CommentCrudController extends AbstractCrudController
     {
         yield AssociationField::new('conference');
         yield TextField::new('author');
+        yield ImageField::new('photoFilename')
+            ->setBasePath('/uploads/photos')
+            ->setLabel('Photo')
+            ->onlyOnIndex();
         yield EmailField::new('email');
         yield TextareaField::new('text')
             ->hideOnIndex()
